9788ed8ed82b377cf8e46f919156420652e0d14c
DK <danielkim@soulkast.com>
2018-01-13 01:16:23 -0500

    api key

--- public/const.js
+++ public/const.js
@@ -1,5 +1,4 @@
 //youtube api related
-const CLIENT_ID = '586555634098-j5ul1cm0c5bj7vo87d8u5volijrneh40.apps.googleusercontent.com';
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
